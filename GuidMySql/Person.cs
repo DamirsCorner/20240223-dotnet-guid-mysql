efdac18da0176634195abc0fc67f705060e2c1db
Damir Arh <damir.arh@gmail.com>
2024-02-08 11:24:49 +0100

    Partial fix with explicit column type mapping

--- GuidMySql/Person.cs
+++ GuidMySql/Person.cs
@@ -1,7 +1,10 @@
-﻿namespace GuidMySql;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuidMySql;
 
 public class Person
 {
+    [Column(TypeName = "binary(16)")]
     public Guid Id { get; set; } = Guid.NewGuid();
     public string FirstName { get; set; }
     public string LastName { get; set; }
